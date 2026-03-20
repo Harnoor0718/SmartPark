59ce32eb8e04775ea3f9ad3d5e43c1ca84f56be1
Aditi Sharma <aditisharma032007@gmail.com>
2026-03-20 23:10:07 +0530

    feat: VehicleDAO + VehicleDAOTest added (Aditi)

---
+++ src/main/java/com/smartpark/VehicleDAO.java
@@ -0,0 +1,29 @@
+package com.smartpark;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VehicleDAO {
+
+    // Save a vehicle to the database
+    public void save(Vehicle vehicle) {
+        // TODO: Connect to DB (Manisha provides connection)
+        System.out.println("Saving vehicle: " + vehicle.getVehicleType() +
+                " | Plate: " + vehicle.getLicensePlate() +
+                " | Owner: " + vehicle.getOwnerId());
+    }
+
+    // Find all vehicles by user ID
+    public List<Vehicle> findByUserId(String userId) {
+        // TODO: Connect to DB (Manisha provides connection)
+        System.out.println("Finding vehicles for user: " + userId);
+        return new ArrayList<>();
+    }
+
+    // Find vehicle by license plate
+    public Vehicle findByLicensePlate(String licensePlate) {
+        // TODO: Connect to DB (Manisha provides connection)
+        System.out.println("Finding vehicle with plate: " + licensePlate);
+        return null;
+    }
+}
---
+++ src/main/java/com/smartpark/VehicleDAOTest.java
@@ -0,0 +1,30 @@
+package com.smartpark;
+
+public class VehicleDAOTest {
+
+    public static void main(String[] args) {
+
+        VehicleDAO dao = new VehicleDAO();
+
+        // Create 3 vehicle types
+        Car car = new Car("V001", "DL1234", "U001");
+        Bike bike = new Bike("V002", "DL5678", "U002");
+        Truck truck = new Truck("V003", "DL9999", "U003");
+
+        // Test save()
+        System.out.println("=== Testing save() ===");
+        dao.save(car);
+        dao.save(bike);
+        dao.save(truck);
+
+        // Test findByUserId()
+        System.out.println("\n=== Testing findByUserId() ===");
+        dao.findByUserId("U001");
+
+        // Test findByLicensePlate()
+        System.out.println("\n=== Testing findByLicensePlate() ===");
+        dao.findByLicensePlate("DL1234");
+
+        System.out.println("\nVehicleDAO test complete! ✅");
+    }
+}
